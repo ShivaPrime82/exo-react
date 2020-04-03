868c0f2c289632096773573f72dafb517529c562
ShivaPrime82 <jeremy.clair.82@gmail.com>
2020-04-03 11:16:13 +0200

    add route for home and notfound

--- src/App.js
+++ src/App.js
@@ -6,9 +6,17 @@ import {
     Route,
 } from 'react-router-dom';
 
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
 function App() {
     return (
-
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="*" component={NotFound} />
+            </Switch>
+        </Router>
     );
 }
 
